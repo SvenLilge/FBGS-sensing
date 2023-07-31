00dbd415f937ba1c7ab30bf14930afc7460bffd9
Sven Lilge <slilge@cs.toronto.edu>
2023-07-31 15:56:15 -0400

    Quick and dirty saving of measurement data

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ Copyright (C) 2022 Sven Lilge, Continuum Robotics Laboratory, University of Toro
 #include <string>
 #include <Eigen/Dense>
 
+#include <fstream>
 
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
@@ -33,53 +34,32 @@ int main(int argc, char **argv)
 	if(!interface.connect())
 		return 0;
 	
+	std::ofstream myfile;
+    myfile.open ("../data.csv");
+	
+	int start = 0;
+	
 	while(1)
 	{
 		if(interface.nextSampleReady())
 		{
+			if(start == 0) {
+				std::cout << "Starting recording!" << std::endl;
+				start++;
+			}
 			ShapeSensingInterface::Sample sample;
 			
 			if(interface.readNextSample(sample))
 			{
-				std::cout << sample.sample_number << std::endl;
-				std::cout << sample.num_channels << std::endl;
-				std::cout << sample.num_sensors << std::endl << std::endl;
-				
-				for(int i = 0; i < sample.num_channels; i++)
-				{
-					std::cout << sample.channels.at(i).channel_number << std::endl;
-					std::cout << sample.channels.at(i).num_gratings << std::endl << std::endl;
-					
-					std::cout << sample.channels.at(i).error_status(0) << std::endl;
-					std::cout << sample.channels.at(i).error_status(1) << std::endl;
-					std::cout << sample.channels.at(i).error_status(2) << std::endl;
-					std::cout << sample.channels.at(i).error_status(3) << std::endl << std::endl;
-					
-					for(int j = 0; j < sample.channels.at(i).num_gratings; j++)
-					{
-						std::cout << sample.channels.at(i).peak_wavelengths(j) << " " << sample.channels.at(i).peak_powers(j) << std::endl;
-					}
-					std::cout << std::endl;
-				}
-				
+				myfile << sample.sample_number << "\n";
 				for(int i = 0; i < sample.num_sensors; i++)
 				{
-					std::cout << sample.sensors.at(i).num_curv_points << std::endl;
-					std::cout << sample.sensors.at(i).num_shape_points << std::endl << std::endl;
-					
-					
-					for(int j = 0; j < sample.sensors.at(i).num_curv_points; j++)
-					{
-						std::cout << sample.sensors.at(i).kappa(j) << " " << sample.sensors.at(i).phi(j) << std::endl;
-					}
-					std::cout << std::endl;
-					
 					
 					for(int j = 0; j < sample.sensors.at(i).num_shape_points; j++)
 					{
-						std::cout << sample.sensors.at(i).shape.row(j) << " " << sample.sensors.at(i).arc_length(j) << std::endl;
+						myfile << sample.sensors.at(i).shape.row(j)(0) << ", "<< sample.sensors.at(i).shape.row(j)(1) << ", "<< sample.sensors.at(i).shape.row(j)(2) << ", ";
 					}
-					std::cout << std::endl;
+					myfile << "\n";
 					
 				}
 			}
